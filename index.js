0fa229c86622e5860935660bf15e45af22ddda92
Md Ahmed <maahmed126@gmail.com>
2021-12-06 22:02:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const { v4: uuidv4 } = require('uuid');
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return uuidv4();
+};
